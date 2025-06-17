0e15857bcadf2601b3e2d56a6238fcc7c4c2a548
Nikos <vassilicopoulos@gmail.com>
2025-06-17 11:57:43 +0300

    fix: update YOLOv3 weights URL to the correct source

--- yolov3/yolo.py
+++ yolov3/yolo.py
@@ -27,7 +27,7 @@ class YOLOv3:
         os.makedirs(os.path.dirname(weights_path), exist_ok=True)
 
         if not os.path.isfile(weights_path):
-            url = 'https://pjreddie.com/media/files/yolov3.weights'
+            url = 'https://data.pjreddie.com/files/yolov3.weights'
             outdir = os.path.dirname(weights_path)
             download_url(url, outdir)
 
